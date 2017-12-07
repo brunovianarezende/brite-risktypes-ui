c0b590df8e4558434e59f5b7581f2b511f4cf36a
Bruno Rezende <brunovianarezende@gmail.com>
2017-12-07 18:17:25 -0200

    Adds DateWidget

---
+++ src/components/DateWidget.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-form-item :label="label">
+    <el-date-picker
+      ref='_datepicker'
+      :placeholder="placeholder"
+      :value="value"
+      type="date"
+      format="dd/MM/yyyy"
+      value-format="yyyy-MM-dd"
+      @change="value => { $emit('input', value) }"
+    ></el-date-picker>
+  </el-form-item>
+</template>
+
+<script>
+export default {
+  name: 'DateWidget',
+  props: ['value', 'label', 'placeholder']
+}
+</script>
