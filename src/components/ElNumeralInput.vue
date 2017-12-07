2698ea463fe1c2ea7f84d03a490f4230fa5408f2
Bruno Rezende <brunovianarezende@gmail.com>
2017-12-07 20:19:06 -0200

    adds NumericWidget

---
+++ src/components/ElNumeralInput.vue
@@ -0,0 +1,119 @@
+<template>
+  <div :class="[
+    type === 'textarea' ? 'el-textarea' : 'el-input',
+    inputSize ? 'el-input--' + inputSize : '',
+    {
+      'is-disabled': disabled,
+      'el-input-group': $slots.prepend || $slots.append,
+      'el-input-group--append': $slots.append,
+      'el-input-group--prepend': $slots.prepend,
+      'el-input--prefix': $slots.prefix || prefixIcon,
+      'el-input--suffix': $slots.suffix || suffixIcon
+    }
+    ]"
+    @mouseenter="hovering = true"
+    @mouseleave="hovering = false"
+  >
+    <template v-if="type !== 'textarea'">
+      <!-- 前置元素 -->
+      <div class="el-input-group__prepend" v-if="$slots.prepend"  tabindex="0">
+        <slot name="prepend"></slot>
+      </div>
+      <input
+        v-if="type !== 'textarea'"
+        class="el-input__inner"
+        v-bind="$props"
+        :autocomplete="autoComplete"
+        :value="currentValue"
+        ref="input"
+        @input="handleInput"
+        @focus="handleFocus"
+        @blur="handleBlur"
+        @change="handleChange"
+        :aria-label="label"
+      >
+      <!-- 前置内容 -->
+      <span class="el-input__prefix" v-if="$slots.prefix || prefixIcon" :style="prefixOffset">
+        <slot name="prefix"></slot>
+        <i class="el-input__icon"
+           v-if="prefixIcon"
+           :class="prefixIcon">
+        </i>
+      </span>
+      <!-- 后置内容 -->
+      <span
+        class="el-input__suffix"
+        v-if="$slots.suffix || suffixIcon || showClear || validateState && needStatusIcon"
+        :style="suffixOffset">
+        <span class="el-input__suffix-inner">
+          <template v-if="!showClear">
+            <slot name="suffix"></slot>
+            <i class="el-input__icon"
+              v-if="suffixIcon"
+              :class="suffixIcon">
+            </i>
+          </template>
+          <i v-else
+            class="el-input__icon el-icon-circle-close el-input__clear"
+            @click="clear"
+          ></i>
+        </span>
+        <i class="el-input__icon"
+          v-if="validateState"
+          :class="['el-input__validateIcon', validateIcon]">
+        </i>
+      </span>
+      <!-- 后置元素 -->
+      <div class="el-input-group__append" v-if="$slots.append">
+        <slot name="append"></slot>
+      </div>
+    </template>
+    <textarea
+      v-else
+      class="el-textarea__inner"
+      :value="currentValue"
+      @input="handleInput"
+      ref="textarea"
+      v-bind="$props"
+      :style="textareaStyle"
+      @focus="handleFocus"
+      @blur="handleBlur"
+      @change="handleChange"
+      :aria-label="label"
+    >
+    </textarea>
+  </div>
+</template>
+<script>
+// NOTE: I just copied the above template code from element-ui since I
+// couldn't find a way to reuse the template from another component :(
+// I'm pretty sure, however, that it must exist a smarter way.
+import { Input } from 'element-ui'
+import Cleave from 'cleave.js'
+
+export default {
+  name: 'ElNumeralInput',
+  extends: Input,
+  computed: {
+    formatter () {
+      return new Cleave.NumeralFormatter(
+                undefined,
+                0,
+                2,
+                'thousand',
+                false,
+                true,
+                ''
+            )
+    }
+  },
+  methods: {
+    handleInput (event) {
+      const value = this.formatter.format(event.target.value)
+      event.target.value = value
+      this.$emit('input', value)
+      this.setCurrentValue(value)
+    }
+  }
+}
+</script>
---
+++ src/components/NumericWidget.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-form-item :label="label">
+    <el-numeral-input
+      :placeholder="placeholder"
+      :value="value"
+      @input="value => { $emit('input', value) }"
+    ></el-numeral-input>
+  </el-form-item>
+</template>
+
+<script>
+import ElNumeralInput from './ElNumeralInput'
+
+export default {
+  name: 'NumericWidget',
+  props: ['value', 'label', 'placeholder'],
+  components: {
+    ElNumeralInput
+  }
+}
+</script>
